1dd296b620bde75bc332fbeab5f7ff1425febe8f
HNCodes-journey <codewithussnain1@gmail.com>
2026-02-04 19:06:46 +0500

    Practice Questions

---
+++ script.js
@@ -0,0 +1,45 @@
+//Practice Questions of Loops
+/*Q1: Print numbers from 1 to 10 using for loop.
+Q2:Print numbers from 10 to 1 using a while loop.
+Q3:Print even numbers from1 to 20 using for loop
+Q4:Print odd numbers from 1 to 15 using while loop.
+Q5:Print the multiplication table of 5 (i.e :- 5*10 = 50)
+
+*/
+//Answer1:
+// for(let i=1 ; i<11 ; i++){
+//     console.log(i);
+// }
+
+//Answer2:
+// let i =10;
+// while(i>0){
+// console.log(i);
+// i--;
+// }
+
+//Answer3:
+// for(let i= 1; i<21 ; i++){
+// if(i%2===0){
+// console.log(i);
+// }
+// }
+
+//Answer4:
+// let i = 1;
+// while(i<16){
+//     if(i%2===1){
+//         console.log(i);
+//     }
+//       i++;
+// }
+//Answer5:
+// for(let i=1 ; i<11 ; i++){
+//     console.log(`5*${i}=${5*i}`);
+// }
+
+// Q6:Print the sum of the numbers using a loop from 1 to 100.
+// Q7:Print all numbers from 1 to 50 that are divisible by 3.
+// Q8:Ask the user for a number and print whether each number from 1 to that number is even or odd.
+//Q9:Count how many numbers between 1 to 100 are divisble by both 3 and 5.
+
